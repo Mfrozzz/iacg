00cda22526d0839b59ecd281a5c0420a91c6996f
Mfrozzz <marcosvinibva@gmail.com>
2024-01-17 15:58:50 -0300

    tabs - botoes no card - select em cada tab -resolver botao fixed

--- js/extras.js
+++ js/extras.js
@@ -10,3 +10,12 @@ document.addEventListener('DOMContentLoaded', function() {
         hoverEnabled: false
     });
   });
+
+//   document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelectorAll('select');
+//     var instances = M.FormSelect.init(elems, options);
+//   });
+
+$(document).ready(function(){
+  $('.tabs').tabs();
+});
