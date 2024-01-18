5fc2386d3e2f9475316a6160533cd0e324431349
Mfrozzz <marcosvinibva@gmail.com>
2024-01-18 15:57:18 -0300

    btns para as questoes, estrutura da lista de arquivos enviados, correcoes para nao quebrar.

--- js/extras.js
+++ js/extras.js
@@ -3,19 +3,19 @@
 //     var instances = M.FloatingActionButton.init(elems, options);
 //   });
 
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.fixed-action-btn');
-    var instances = M.FloatingActionButton.init(elems,{
-        direction: 'left',
-        hoverEnabled: false
-    });
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.fixed-action-btn');
+  var instances = M.FloatingActionButton.init(elems, {
+    direction: 'left',
+    hoverEnabled: false
   });
+});
 
 //   document.addEventListener('DOMContentLoaded', function() {
 //     var elems = document.querySelectorAll('select');
 //     var instances = M.FormSelect.init(elems, options);
 //   });
 
-$(document).ready(function(){
+$(document).ready(function () {
   $('.tabs').tabs();
 });
