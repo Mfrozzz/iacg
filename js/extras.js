11dfae50d0a741047d279513be39a694951802de
Mfrozzz <marcosvinibva@gmail.com>
2024-01-24 11:56:19 -0300

    botão questão respondida,  tentativa de fix no bug da tab

--- js/extras.js
+++ js/extras.js
@@ -61,50 +61,91 @@ input.addEventListener("change", function () {
 var tab_content = document.querySelectorAll("#tab-content-visible");
 var indicador = document.getElementsByClassName("indicator");
 var ativar = document.getElementsByClassName("active");
-var tab_class = document.getElementsByClassName("destaqueTab");
-function showAndDisplay(){
-    for(let i = 0;i<tab_content.length;i++){
-      if(tab_content[i].style.display == "flex"){
-        tab_content[i].style.display = "none"
-      }else{
-        tab_content[i].style.display = "flex";
-      }
+// var tab_class = document.getElementsByClassName("destaqueTab");
+var tab1 = false;
+var tab2 = false;
+var tab3 = false;
+function showAndDisplay() {
+  tab1 = false;
+  tab2 = false;
+  tab3 = false;
+  for (let i = 0; i < tab_content.length; i++) {
+    if (tab_content[i].style.display == "flex") {
+      tab_content[i].style.display = "none";
+    }else{
+      tab_content[i].style.display = "flex";
     }
-    for(let i =0;i<indicador.length;i++){
-      indicador[i].style.display = "block";
-    }
-    for(let i =0;i<ativar.length;i++){
-      ativar[i].style.display = "block";
-    }
+  }
+  //   if (tab_content[i].style.display == "flex") {
+  //     if ($("#tab1").hasClass("active")) {
+  //       tab1 = true;
+  //     }
+  //     if ($("#tab2").hasClass("active")) {
+  //       tab2 = true;
+  //     }
+  //     if ($("#tab3").hasClass("active")) {
+  //       tab3 = true;
+  //     }
+  //   } else {
+  //     tab_content[i].style.display = "flex";
+  //     tab1 = false;
+  //     tab2 = false;
+  //     tab3 = false;
+  //   }
+  // }
+  // console.log(tab1 + " tab1");
+  // console.log(tab2 + " tab2");
+  // console.log(tab3 + " tab3");
+  // if ((tab1 && !tab2 && !tab3) && (tab_content[0].style.display == "flex")) {
+  //   tab_content[0].style.display = "none";
+  //   tab1 = false;
+  // }
+  // if ((tab2 && !tab1 && !tab3) && (tab_content[1].style.display == "flex")) {
+  //   tab_content[1].style.display = "none";
+  //   tab2 = false;
+  // }
+  // if ((tab3 && !tab1 && !tab2) && (tab_content[2].style.display == "flex")) {
+  //   tab3 = false;
+  //   tab_content[2].style.display = "none";
+  // }
+  // for (let i = 0; i < indicador.length; i++) {
+  //   indicador[i].style.display = "block";
+  // }
+  // for (let i = 0; i < ativar.length; i++) {
+  //   ativar[i].style.display = "block";
+  // }
+  // console.log(tab1 + " tab1");
+  // console.log(tab2 + " tab2");//talvez jogar pra cá
+  // console.log(tab3 + " tab3");
 }
 
 // ----- TROCA número QUESTÕES e seus estilos----- //
 var numeroQuestao = document.getElementsByClassName("itemQuestao");
 var dimensaoQuestao = document.getElementsByClassName("tab-style-background");
 // console.log(numeroQuestao[0].innerText);
-function navQuest(e){
+function navQuest(e) {
   // string to int, separa as partes inteira e flutuante para usar na navegação;
   var splitted = e.split('.');
   var inteiro = parseInt(splitted[0]);
   var decimal = parseInt(splitted[1] || 0);
-  decimal = decimal-1;
+  decimal = decimal - 1;
   dimensaoQuestao[0].classList.remove("destaqueTab");
   dimensaoQuestao[1].classList.remove("destaqueTab");
   dimensaoQuestao[2].classList.remove("destaqueTab");
   // estilos do botão da questão atual e tab dessa questão
-  for(let i=0;i<numeroQuestao.length;i++){
+  for (let i = 0; i < numeroQuestao.length; i++) {
     numeroQuestao[i].classList.remove("destaqueBtn");
-    if(inteiro == 1){
+    if (inteiro == 1) {
       numeroQuestao[decimal].classList.add("destaqueBtn");
       dimensaoQuestao[0].classList.add("destaqueTab");
     }
-    if(inteiro == 2){
-      numeroQuestao[decimal+24].classList.add("destaqueBtn");
+    if (inteiro == 2) {
+      numeroQuestao[decimal + 24].classList.add("destaqueBtn");
       dimensaoQuestao[1].classList.add("destaqueTab");
     }
-    if(inteiro == 3){
+    if (inteiro == 3) {
       dimensaoQuestao[2].classList.add("destaqueTab");
-      numeroQuestao[decimal+40].classList.add("destaqueBtn");
+      numeroQuestao[decimal + 40].classList.add("destaqueBtn");
     }
   }
   // string do numero da questão alterado
