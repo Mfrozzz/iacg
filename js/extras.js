bb100452c9d5ca4c5c251fcb52b1ed77c0bec80d
Mfrozzz <marcosvinibva@gmail.com>
2024-01-25 15:51:37 -0300

    limpando, comentando e formatando códigos

--- js/extras.js
+++ js/extras.js
@@ -66,19 +66,19 @@ document.querySelectorAll("#tabs-parent-list > li").forEach(tab => {
   //para cada tab captura o evento de click
   tab.addEventListener("click", (e) => {
     var indicador = document.querySelector(".indicator");
-    if(tabAtiva === null){
+    if (tabAtiva === null) {
       // caso flag === null switch ON, atribui á flag o id da tab e deixa o indicador de tabs visivel.
       tab_content.classList.toggle("aberto");
       tabAtiva = e.target.id;
       indicador.style.display = "block";
-      tabsParent.setAttribute("title","Clique para fechar navegação.");
-    }else if(tabAtiva == e.target.id){
+      tabsParent.setAttribute("title", "Clique para fechar navegação.");
+    } else if (tabAtiva == e.target.id) {
       // caso a flag tenha id da tab clicada anteriormente, esta volta a ser null e indicador recebe display: none;
       tab_content.classList.toggle("aberto");
       tabAtiva = null;
       indicador.style.display = "none";
-      tabsParent.setAttribute("title","Clique para navegar entre as Questões.");
-    }else{
+      tabsParent.setAttribute("title", "Clique para navegar entre as Questões.");
+    } else {
       tabAtiva = e.target.id;
     }
   });
@@ -89,32 +89,32 @@ var numeroQuestao = document.getElementsByClassName("itemQuestao");
 var dimensaoQuestao = document.getElementsByClassName("tab-style-background");
 // var btnAtivo = null;
 document.querySelectorAll(".itemQuestao").forEach(btn => {
-  btn.addEventListener("click",(e) => {
-  // string to int, separa as partes inteira e flutuante para usar na navegação;
-  var splitted = e.target.innerText.split('.');
-  var inteiro = parseInt(splitted[0]); // dimensão
-  var decimal = parseInt(splitted[1] || 0); // questão
-  decimal = decimal - 1;
-  for(let i =0;i<dimensaoQuestao.length;i++){
-    dimensaoQuestao[i].classList.remove("destaqueTab");
-  }
-  // estilos do botão da questão atual e tab dessa questão
-  for (let i = 0; i < numeroQuestao.length; i++) {
-    numeroQuestao[i].classList.remove("destaqueBtn");
-    if (inteiro == 1) {
-      numeroQuestao[decimal].classList.add("destaqueBtn");
-      dimensaoQuestao[0].classList.add("destaqueTab");
+  btn.addEventListener("click", (e) => {
+    // string to int, separa as partes inteira e flutuante para usar na navegação;
+    var splitted = e.target.innerText.split('.');
+    var inteiro = parseInt(splitted[0]); // dimensão
+    var decimal = parseInt(splitted[1] || 0); // questão
+    decimal = decimal - 1;
+    for (let i = 0; i < dimensaoQuestao.length; i++) {
+      dimensaoQuestao[i].classList.remove("destaqueTab");
     }
-    if (inteiro == 2) {
-      numeroQuestao[decimal + 24].classList.add("destaqueBtn");
-      dimensaoQuestao[1].classList.add("destaqueTab");
+    // estilos do botão da questão atual e tab dessa questão
+    for (let i = 0; i < numeroQuestao.length; i++) {
+      numeroQuestao[i].classList.remove("destaqueBtn");
+      if (inteiro == 1) {
+        numeroQuestao[decimal].classList.add("destaqueBtn");
+        dimensaoQuestao[0].classList.add("destaqueTab");
+      }
+      if (inteiro == 2) {
+        numeroQuestao[decimal + 24].classList.add("destaqueBtn");
+        dimensaoQuestao[1].classList.add("destaqueTab");
+      }
+      if (inteiro == 3) {
+        dimensaoQuestao[2].classList.add("destaqueTab");
+        numeroQuestao[decimal + 40].classList.add("destaqueBtn");
+      }
     }
-    if (inteiro == 3) {
-      dimensaoQuestao[2].classList.add("destaqueTab");
-      numeroQuestao[decimal + 40].classList.add("destaqueBtn");
-    }
-  }
-  // string do numero da questão alterado
-  document.getElementById("textoQuest").innerHTML = btn.innerText;
+    // string do numero da questão alterado
+    document.getElementById("textoQuest").innerHTML = btn.innerText;
   });
 });
