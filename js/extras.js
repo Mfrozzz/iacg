3caff41765084f90c0e29b6174e54eb96b3bf80c
Mfrozzz <marcosvinibva@gmail.com>
2024-01-25 11:44:28 -0300

    tabs resolvidas -- gambiarras retiradas -- documentação por comentários

--- js/extras.js
+++ js/extras.js
@@ -12,7 +12,6 @@ $(document).ready(function () {
   $('.tabs').tabs();
 });
 
-
 // ----- INPUT FILE CONTROLLER ----- //
 var div = document.getElementsByClassName("botao-arquivo")[0];
 var input = document.getElementById("input-arquivo");
@@ -28,16 +27,18 @@ input.addEventListener("change", function () {
   var lista = document.getElementById('lista');
   if (input.files.length > 0) {
     nome = input.files[0].name;
+    // joga nome dos files recebidos no input file em um array dinâmico
     arquivos.push(nome);
     for (var i = 0; i < arquivos.length; i++) {
+      // no elemento ul#lista é adicionado um item(li)
       let item = document.createElement('li');
       item.setAttribute("id", "list");
       lista.appendChild(item);
-
+      // neste li é adiconado uma div
       let container = document.createElement("div");
       container.setAttribute("id", "box");
       container.setAttribute("class", "card-panel correct-card-padding")
-
+      // nesta div é adicionado um texto(P) e um link(A)--> neste link um icone
       let texto = document.createElement("p");
       texto.appendChild(document.createTextNode(arquivos[i]));
       let link = document.createElement("a");
@@ -45,7 +46,7 @@ input.addEventListener("change", function () {
       let icone = document.createElement("i");
       icone.setAttribute("class", "material-icons")
       icone.appendChild(document.createTextNode("delete_forever"));
-
+      // todos são adicionados ao li dentro do ul e mostrados na prancheta de anexos.
       item.appendChild(container);
       container.appendChild(texto);
       container.appendChild(link)
@@ -59,103 +60,44 @@ input.addEventListener("change", function () {
 
 // ----- VISIBILIDADE DAS QUESTÕES ----- //
 var tab_content = document.querySelector("#tab-content");
-var indicador = document.getElementsByClassName("indicator");
-
+var tabsParent = document.getElementById("tabs-parent-list");
+var tabAtiva = null; //funciona como uma flag, pega id da tab e só fecha ela caso clique na mesma estando esta aberta
 document.querySelectorAll("#tabs-parent-list > li").forEach(tab => {
+  //para cada tab captura o evento de click
   tab.addEventListener("click", (e) => {
-    tab_content.classList.toggle("aberto");
-    console.log(this.querySelector("a").classList)
-    //event.target
-    // if(tab_content.style.display == "block"){
-    //   tab_content.style.display = "none";
-    // }else{
-    //   tab_content.style.display = "block";
-    // }
-
-    for (let i = 0; i < indicador.length; i++) {
-      indicador[i].style.display = "block";
+    var indicador = document.querySelector(".indicator");
+    if(tabAtiva === null){
+      // caso flag === null switch ON, atribui á flag o id da tab e deixa o indicador de tabs visivel.
+      tab_content.classList.toggle("aberto");
+      tabAtiva = e.target.id;
+      indicador.style.display = "block";
+      tabsParent.setAttribute("title","Clique para fechar navegação.");
+    }else if(tabAtiva == e.target.id){
+      // caso a flag tenha id da tab clicada anteriormente, esta volta a ser null e indicador recebe display: none;
+      tab_content.classList.toggle("aberto");
+      tabAtiva = null;
+      indicador.style.display = "none";
+      tabsParent.setAttribute("title","Clique para navegar entre as Questões.");
+    }else{
+      tabAtiva = e.target.id;
     }
-
   });
 });
-// var tab1 = document.getElementById("tab1");
-// var tab2 = document.getElementById("tab2");
-// var tab3 = document.getElementById("tab3");
-// var ativo = [false,false,false];
-
-// function showAndDisplay() {
-//   // tab1.onclick = function(){
-//   //   if(tab_content[0].style.display === "flex" && ativo[0]){
-//   //     tab_content[0].style.display = "none";
-//   //     ativo[0] = false;
-//   //     ativo[1] = true;
-//   //     ativo[2] = true;
-//   //   }else{
-//   //     tab_content[0].style.display = "flex";
-//   //     ativo[0] = true;
-//   //     ativo[1] = false;
-//   //     ativo[2] = false;
-//   //   }
-//   // };
-//   // tab2.onclick = function(){
-//   //   if(tab_content[1].style.display === "flex" && ativo[1]){
-//   //     tab_content[1].style.display = "none";
-//   //     ativo[0] = true;
-//   //     ativo[1] = false;
-//   //     ativo[2] = true;
-//   //   }else{
-//   //     tab_content[1].style.display = "flex";
-//   //     ativo[0] = false;
-//   //     ativo[1] = true;
-//   //     ativo[2] = false;
-//   //   }
-//   // };
-//   // tab3.onclick = function(){
-//   //   if(tab_content[2].style.display === "flex" && ativo[2]){
-//   //     tab_content[2].style.display = "none";
-//   //     ativo[0] = true;
-//   //     ativo[1] = true;
-//   //     ativo[2] = false;
-//   //   }else{
-//   //     tab_content[2].style.display = "flex";
-//   //     ativo[0] = false;
-//   //     ativo[1] = false;
-//   //     ativo[2] = true;
-//   //   }
-//   // };
-//   // for (let i = 0; i < tab_content.length; i++) {
-//   //   if (tab_content[i].style.display == "flex") {
-//   //     tab_content[i].style.display = "none";
-//   //   }else{
-//   //     tab_content[i].style.display = "flex";
-//   //   }
-//   // }
-
-//   tab_content.forEach(e => {
-
-//     // if(e.style.display == "flex"){
-//     //   e.style.display = "none";
-//     // }else{
-//     //   e.style.display = "flex";
-//     // }
-//   });
-
-
-// }
 
 // ----- TROCA número QUESTÕES e seus estilos----- //
 var numeroQuestao = document.getElementsByClassName("itemQuestao");
 var dimensaoQuestao = document.getElementsByClassName("tab-style-background");
-// console.log(numeroQuestao[0].innerText);
-function navQuest(e) {
+// var btnAtivo = null;
+document.querySelectorAll(".itemQuestao").forEach(btn => {
+  btn.addEventListener("click",(e) => {
   // string to int, separa as partes inteira e flutuante para usar na navegação;
-  var splitted = e.split('.');
-  var inteiro = parseInt(splitted[0]);
-  var decimal = parseInt(splitted[1] || 0);
+  var splitted = e.target.innerText.split('.');
+  var inteiro = parseInt(splitted[0]); // dimensão
+  var decimal = parseInt(splitted[1] || 0); // questão
   decimal = decimal - 1;
-  dimensaoQuestao[0].classList.remove("destaqueTab");
-  dimensaoQuestao[1].classList.remove("destaqueTab");
-  dimensaoQuestao[2].classList.remove("destaqueTab");
+  for(let i =0;i<dimensaoQuestao.length;i++){
+    dimensaoQuestao[i].classList.remove("destaqueTab");
+  }
   // estilos do botão da questão atual e tab dessa questão
   for (let i = 0; i < numeroQuestao.length; i++) {
     numeroQuestao[i].classList.remove("destaqueBtn");
@@ -173,5 +115,6 @@ function navQuest(e) {
     }
   }
   // string do numero da questão alterado
-  document.getElementById("textoQuest").innerHTML = e;
-}
+  document.getElementById("textoQuest").innerHTML = btn.innerText;
+  });
+});
