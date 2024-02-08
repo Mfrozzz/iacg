efabbc25b22de6ff41a4b7d07733935b4e3717c6
Mfrozzz <marcosvinibva@gmail.com>
2024-02-08 11:00:33 -0300

    troca de arquivos materialize pelo CDN e Readme atualizado

--- README.md
+++ README.md
@@ -6,3 +6,62 @@
 
 > Marcos Vinicius Schimaichel Boava
 >> mboava@unicentro.br
+
+## Capturas de tela do projeto
+
+### Conteúdo da Tela do questionário
+
+<div style="width:90%;display: inline-block;" align="center">
+    <img alt="Header" src="./prints/telasIacg (1).png">
+    <p>Header + Conteúdo.</p>
+</div>
+
+<br>
+
+<div style="width:90%;display: inline-block;" align="center">
+    <img alt="Footer" src="./prints/telasIacg (2).png">
+    <p>Footer + Conteúdo.</p>
+</div>
+
+### Navegação Entre Questões
+
+<div style="width:90%;display: inline-block;" align="center">
+    <img alt="Questão 1.1" src="./prints/telasIacg (3).png">
+    <p>Questão 1.1 Selecionada.</p>
+</div>
+
+<br>
+
+<div style="width:90%;display: inline-block;" align="center">
+    <img alt="Seleção 2.16" src="./prints/telasIacg (4).png">
+    <p>Seleção de questão em outra dimensão.</p>
+</div>
+
+<br>
+
+<div style="width:90%;display: inline-block;" align="center">
+    <img alt="Visualização mas com outra dimensão selecionada" src="./prints/telasIacg (5).png">
+    <p>Visualização de outra Dimensão, mas com questão 2.16 selecionada.</p>
+    <p>Detaque na Dimensão 2, navegação na Dimensão 3.</p>
+</div>
+
+### Modals 
+
+<div style="width:90%;display: inline-block;" align="center">
+    <img alt="Modal Info" src="./prints/telasIacg (6).png">
+    <p>Modal Informações gerais.</p>
+</div>
+
+<br>
+
+<div style="width:90%;display: inline-block;" align="center">
+    <img alt="Modal Prancheta" src="./prints/telasIacg (7).png">
+    <p>Prancheta de anexos Vazia</p>
+</div>
+
+<br>
+
+<div style="width:90%;display: inline-block;" align="center">
+    <img alt="Modal Prancheta" src="./prints/telasIacg (8).png">
+    <p>Prancheta de anexos com Conteúdos</p>
+</div>
--- js/scripts.js
+++ js/scripts.js
@@ -1,57 +1,120 @@
-$(document).ready(function(){
-    $('.tooltipped').tooltip({delay: 50});
- 
-});
-
-function criarPDFAnotacao(){
-    var anotacao = document.getElementById('anotacao').value;
-
-    var win = window.open('', '', 'height=1000,width=1000');
-    win.document.write('<html><head>');
-    win.document.write('<title>Anotações</title>');                                       
-    win.document.write('</head>');
-    win.document.write('<body>');
-    win.document.write('<p><b>Anotação</b><p>');
-    win.document.write('<br>');
-    win.document.write(anotacao);                         
-    win.document.write('</body></html>');
-    win.document.close(); 	                                         
-    win.print();                                
-}
-
-function criarPDF(){
-    var tema = document.getElementById('tema').value;
-    var titulo = document.getElementById('titulo').value;
-    var introducao = document.getElementById('introducao').value;
-    var problema = document.getElementById('problema').value;
-    var objetivos = document.getElementById('objetivos').value;
-    var justificativa = document.getElementById('justificativa').value;
-    var referencial = document.getElementById('referencial').value;
-
-    var style = '';
-
-    // CRIA UM OBJETO WINDOW
-    var win = window.open('', '', 'height=1000,width=1000');
-    win.document.write('<html><head>');
-    win.document.write('<title>Proposta de Pesquisa</title>');                                       
-    win.document.write('</head>');
-    win.document.write('<body>');
-    win.document.write('<p><b>Proposta de Pesquisa</b><p>');
-    win.document.write('<br><p>Tema:</p>');
-    win.document.write(tema);
-    win.document.write('<br><p>Título:</p>');
-    win.document.write(titulo);
-    win.document.write('<br><p>Contexto da Realidade Investigada::</p>');
-    win.document.write(introducao); 
-    win.document.write('<br><p>Problema:</p>');
-    win.document.write(problema); 
-    win.document.write('<br><p>Objetivos:</p>');
-    win.document.write(objetivos); 
-    win.document.write('<br><p>Justificativa:</p>');
-    win.document.write(justificativa); 
-    win.document.write('<br><p>Referencial:</p>');
-    win.document.write(referencial);                          
-    win.document.write('</body></html>');
-    win.document.close(); 	                                         
-    win.print();                                                          
-}
+// ----- BOTÃO DE ANEXOS ----- //
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems, {
+      direction: 'left',
+      hoverEnabled: false
+    });
+  });
+  
+  // ----- INICIALIZADOR DE TABS ----- //
+  $(document).ready(function () {
+    $('.tabs').tabs();
+  });
+  
+  // ----- INPUT FILE CONTROLLER ----- //
+  var div = document.getElementsByClassName("botao-arquivo")[0];
+  var input = document.getElementById("input-arquivo");
+  let clone_arquivos, clone_lista;
+  
+  div.addEventListener("click", function () {
+    input.click();
+  });
+  // ----- CRIAÇÃO LI NA PRANCHETA DE ANEXOS DINÂMICAMENTE ----- //
+  input.addEventListener("change", function () {
+    var nome = "Não há arquivo selecionado. Selecionar arquivo...";
+    var arquivos = [];
+    var lista = document.getElementById('lista');
+    if (input.files.length > 0) {
+      nome = input.files[0].name;
+      // joga nome dos files recebidos no input file em um array dinâmico
+      arquivos.push(nome);
+      for (var i = 0; i < arquivos.length; i++) {
+        // no elemento ul#lista é adicionado um item(li)
+        let item = document.createElement('li');
+        item.setAttribute("id", "list");
+        lista.appendChild(item);
+        // neste li é adiconado uma div
+        let container = document.createElement("div");
+        container.setAttribute("id", "box");
+        container.setAttribute("class", "card-panel correct-card-padding")
+        // nesta div é adicionado um texto(P) e um link(A)--> neste link um icone
+        let texto = document.createElement("p");
+        texto.appendChild(document.createTextNode(arquivos[i]));
+        let link = document.createElement("a");
+        link.setAttribute("id", "link");
+        let icone = document.createElement("i");
+        icone.setAttribute("class", "material-icons")
+        icone.appendChild(document.createTextNode("delete_forever"));
+        // todos são adicionados ao li dentro do ul e mostrados na prancheta de anexos.
+        item.appendChild(container);
+        container.appendChild(texto);
+        container.appendChild(link)
+        link.appendChild(icone);
+      }
+    }
+    div.innerHTML = nome;
+    clone_arquivos = arquivos;
+    clone_lista = lista;
+  });
+  
+  // ----- VISIBILIDADE DAS QUESTÕES ----- //
+  var tab_content = document.querySelector("#tab-content");
+  var tabsParent = document.getElementById("tabs-parent-list");
+  var tabAtiva = null; //funciona como uma flag, pega id da tab e só fecha ela caso clique na mesma estando esta aberta
+  document.querySelectorAll("#tabs-parent-list > li").forEach(tab => {
+    //para cada tab captura o evento de click
+    tab.addEventListener("click", (e) => {
+      var indicador = document.querySelector(".indicator");
+      if (tabAtiva === null) {
+        // caso flag === null switch ON, atribui á flag o id da tab e deixa o indicador de tabs visivel.
+        tab_content.classList.toggle("aberto");
+        tabAtiva = e.target.id;
+        indicador.style.display = "block";
+        tabsParent.setAttribute("title", "Clique para fechar navegação.");
+      } else if (tabAtiva == e.target.id) {
+        // caso a flag tenha id da tab clicada anteriormente, esta volta a ser null e indicador recebe display: none;
+        tab_content.classList.toggle("aberto");
+        tabAtiva = null;
+        indicador.style.display = "none";
+        tabsParent.setAttribute("title", "Clique para navegar entre as Questões.");
+      } else {
+        tabAtiva = e.target.id;
+      }
+    });
+  });
+  
+  // ----- TROCA número QUESTÕES e seus estilos----- //
+  var numeroQuestao = document.getElementsByClassName("itemQuestao");
+  var dimensaoQuestao = document.getElementsByClassName("tab-style-background");
+  // var btnAtivo = null;
+  document.querySelectorAll(".itemQuestao").forEach(btn => {
+    btn.addEventListener("click", (e) => {
+      // string to int, separa as partes inteira e flutuante para usar na navegação;
+      var splitted = e.target.innerText.split('.');
+      var inteiro = parseInt(splitted[0]); // dimensão
+      var decimal = parseInt(splitted[1] || 0); // questão
+      decimal = decimal - 1;
+      for (let i = 0; i < dimensaoQuestao.length; i++) {
+        dimensaoQuestao[i].classList.remove("destaqueTab");
+      }
+      // estilos do botão da questão atual e tab dessa questão
+      for (let i = 0; i < numeroQuestao.length; i++) {
+        numeroQuestao[i].classList.remove("destaqueBtn");
+        if (inteiro == 1) {
+          numeroQuestao[decimal].classList.add("destaqueBtn");
+          dimensaoQuestao[0].classList.add("destaqueTab");
+        }
+        if (inteiro == 2) {
+          numeroQuestao[decimal + 24].classList.add("destaqueBtn");
+          dimensaoQuestao[1].classList.add("destaqueTab");
+        }
+        if (inteiro == 3) {
+          dimensaoQuestao[2].classList.add("destaqueTab");
+          numeroQuestao[decimal + 40].classList.add("destaqueBtn");
+        }
+      }
+      // string do numero da questão alterado
+      document.getElementById("textoQuest").innerHTML = btn.innerText;
+    });
+  });
